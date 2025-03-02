6bcce87ad95fbeefffa751583c29b019a12fe032
mat <matdallen@gmail.com>
2025-03-01 18:38:56 -0800

    hotkey is working

--- fact2clozeBEEF.sh
+++ fact2clozeBEEF.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-source /home/mat/Documents/ProgramExperiments/fact2cloze/f2cenv/bin/activate
-python /home/mat/Documents/ProgramExperiments/fact2cloze/beef.py
+PROGRAM_DIR="/home/mat/Documents/ProgramExperiments/fact2cloze"
+LOG_FILE="$PROGRAM_DIR/log.log"
+
+source $PROGRAM_DIR/f2cenv/bin/activate
+python $PROGRAM_DIR/beef.py
 deactivate
